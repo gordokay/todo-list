edd6a8d2be12b95a55fab6dace776aa1215963a1
Kayleigh <57508868+gordokay@users.noreply.github.com>
2022-12-23 11:14:17 -0500

    Add Task class

---
+++ src/task.js
@@ -0,0 +1,14 @@
+class Task {
+  constructor(title, desc, dueDate, priority, notes, checklist) {
+    this.title = title;
+    this.desc = desc;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+  }
+
+  addToChecklist(item) {
+    this.checklist.append(item);
+  }
+}
