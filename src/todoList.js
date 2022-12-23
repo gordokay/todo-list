ce3998d6bb1f58498639d4ea6b166ea61272b4af
Kayleigh <57508868+gordokay@users.noreply.github.com>
2022-12-23 11:25:40 -0500

    Add TodoList class

---
+++ src/todoList.js
@@ -0,0 +1,13 @@
+class TodoList {
+  constructor() {
+    this.projects = [];
+  }
+
+  addProject(project) {
+    this.projects.push(project);
+  }
+
+  deleteProject(project) {
+    this.projects.splice(this.projects.indexOf(project), 1);
+  }
+}
